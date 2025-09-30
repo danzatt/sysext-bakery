007a48bf82337f8d99a0ce67315c89d9ca987458
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-09-30 17:54:07 +0200

    [WIP]: Add sysext bakery signing

---
+++ flatcar-container-wrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONTAINER_IMAGE=$1
+shift
+
+ARGS=("--rm" "--volume" "$PWD:/app" --entrypoint "" --workdir "/app" "--privileged")
+# Extra "sh -c" is needed to only export the exported variables
+for VARNAME in $(bash -c 'compgen -v | grep -vE "(DIRSTACK|HOME|HOSTNAME|LOGNAME|MAIL|OLDPWD|PATH|PWD|USER|USERNAME|XDG_DATA_DIRS|BASH|EPOCH|RANDOM)"'); do
+  set +u
+  VAL="${!VARNAME}"
+  set -u
+  ARGS+=("--env" "${VARNAME}=${VAL}")
+done
+
+# echo "${ARGS[@]}"
+# sudo docker run "${ARGS[@]}" --entrypoint "" "$CONTAINER_IMAGE" env
+
+docker run "${ARGS[@]}" "$CONTAINER_IMAGE" "$@"
