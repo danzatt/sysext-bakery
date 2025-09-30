af5353923bc2a9c7079b12e77b9944fb0b6f1d2d
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-09-30 17:54:07 +0200

    [WIP]: Add sysext bakery signing

---
+++ setup_scripts_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git -C /tmp clone --depth=1 https://github.com/flatcar/scripts.git -b danzatt/sign-sysexts-nopatch
+sudo rm -rf /mnt/host/source
+sudo mkdir -p /tmp/scripts/__build__/images
+
+sudo ln -s /tmp/scripts/sdk_container /mnt/host/source
+sudo ln -s /tmp/scripts/__build__/images /mnt/host/source/src/build
+sudo ln -s /tmp/scripts/ /mnt/host/source/src/scripts
+sudo emerge -U systemd
+sudo emerge -U azure-keyvault-pkcs11
+
+touch /etc/pkcs11/pkcs11.conf
+echo "module: /usr/lib64/pkcs11/azure-keyvault-pkcs11.so" | sudo tee /etc/pkcs11/modules/azure-keyvault-pkcs11.module
