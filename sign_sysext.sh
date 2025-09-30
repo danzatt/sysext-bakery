69fc2e386de95bb07ad946d82e09409880bf10c2
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-09-30 17:54:07 +0200

    [WIP]: Add sysext bakery signing

---
+++ sign_sysext.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+
+# set -ex
+set -euo pipefail
+shopt -s nullglob
+set -x
+
+function fail() {
+  echo "$@"
+  exit 1
+}
+
+function generate_repart_config() {
+  FS_IMAGE="$1"
+
+  # Create temporary working directory
+  WORKDIR=$(mktemp -d)
+
+  cat > "$WORKDIR/10-root.conf" <<EOF
+[Partition]
+Type=root
+Verity=data
+VerityMatchKey=root
+CopyBlocks=$FS_IMAGE
+EOF
+
+  # Optionally add verity partition
+  cat > "$WORKDIR/20-verity.conf" <<EOF
+[Partition]
+Type=root-verity
+Verity=hash
+VerityMatchKey=root
+Minimize=best
+EOF
+
+  # Optionally add signature partition
+  cat > "$WORKDIR/30-signature.conf" <<EOF
+[Partition]
+Type=root-verity-sig
+Verity=signature
+VerityMatchKey=root
+EOF
+  echo "$WORKDIR"
+}
+
+function usage() {
+    echo "Usage: $0 <sysext_name> <private_key_spec> [<cert_path>]"
+    exit 1
+}
+
+function sign_sysext() {
+  FS_IMAGE="$1"
+  OUTPUT_IMAGE="$2"
+  KEY_SPEC="$3"
+  CERT_SPEC="$4"
+
+  REPART_CONFIG_PATH=$(generate_repart_config "$FS_IMAGE")
+  trap 'rm -rf "$REPART_CONFIG_PATH"' EXIT
+  echo "Generated config in $REPART_CONFIG_PATH"
+  ls $REPART_CONFIG_PATH
+  cat ${REPART_CONFIG_PATH}/10-root.conf
+
+  env | grep AZURE
+
+  systemd-repart \
+    --empty=create \
+    --size=auto \
+    --private-key="$KEY_SPEC" \
+    --certificate="$CERT_SPEC" \
+    --definitions="$REPART_CONFIG_PATH" \
+    --private-key-source=engine:pkcs11 \
+    --certificate-source=provider:pkcs11 \
+   "$OUTPUT_IMAGE"
+}
+
+if [[ "$#" -lt 2 || "$#" -gt 3 ]]; then
+    usage
+fi
+
+SYSEXT_NAME="$1"
+
+KEY_SPEC="$2"
+if [[ "$#" -eq 2 && ${KEY_SPEC} != pkcs11:* ]]; then
+  fail "You have to specify cert_path when not using PKCS11 token."
+fi
+CERT_NAME="${3:-${KEY_SPEC};type=cert}"
+
+for raw_image in "${SYSEXT_NAME}"*.raw; do
+  echo "Should sign $raw_image with key $KEY_SPEC and cert $CERT_NAME"
+  signed_image_path="${raw_image%.raw}-signed-ddi.raw"
+  raw_image=$(readlink -f "$raw_image")
+  sign_sysext "$raw_image" "$signed_image_path" "$KEY_SPEC" "$CERT_NAME"
+done
+
+for conf_file in "$SYSEXT_NAME"*.conf; do
+  echo "Modifying sysupdate config $conf_file"
+  sed -E 's/^(MatchPattern.*)\.raw/\1-signed-ddi.raw/g' > "${conf_file%.conf}-signed-ddi.conf" < "$conf_file"
+done
